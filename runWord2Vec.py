35769c2d9231f6df24da68cc690e71da53603fd5
fzohrasmaili <34123475+fzohrasmaili@users.noreply.github.com>
2019-03-12 14:56:16 +0300

    Update runWord2Vec.py

--- runWord2Vec.py
+++ runWord2Vec.py
@@ -22,7 +22,7 @@ mymodel.min_count = mincount
 sentences =gensim.models.word2vec.LineSentence('ontology_corpus.lst')
 mymodel.build_vocab(sentences, update=True)
 #mymodel =gensim.models.Word2Vec(sentences,sg=0,min_count=0, size=200 ,window=5, sample=1e-3)
-mymodel.train (sentences,total_examples=mymodel.corpus_count, epochs=mymodel.iter)
+mymodel.train (sentences,total_examples=mymodel.corpus_count, epochs=100)
 #print (len(mymodel.wv.vocab));
 # Store vectors for each given class
 word_vectors=mymodel.wv
